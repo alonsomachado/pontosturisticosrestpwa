a90fb61dcc4f7e96aa1dd39f6a4abbc34b82ed1a
Alonso Lima Machado <alonsomachado2@gmail.com>
2020-07-15 19:51:50 +0100

    Heroku deploy2
    Heroku deploy2

--- src/main/java/estg/ipp/articles/StatisticRepository.java
+++
@@ -1,9 +0,0 @@
-package estg.ipp.articles;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface StatisticRepository extends JpaRepository<Statistic, String> {
-//public interface StatisticRepository extends ReactiveCrudRepository<Statistic, String> {
-}
